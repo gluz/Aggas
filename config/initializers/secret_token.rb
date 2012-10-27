ede93ecc1054a6427640fc9714f6433f75ff654b
Gil <gluz@windowslive.com>
2012-10-27 22:14:58 +0200

    mongoid setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aggas::Application.config.secret_token = '0c688982e08af1f5ae37edf985a779966f69852666aacb794cdd9c0341a0b4ec78ed8bc3556b0f9803048c9376773dba0c8c739fe2efa0b655f17fd6865b9cfc'
